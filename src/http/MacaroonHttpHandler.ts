0ce87d27c94c2666bb05474762708ab0c0234824
Ruben Lauwaert <rubenlauwaert1999@gmail.com>
2023-03-16 20:14:39 +0100

    .

--- src/http/MacaroonHttpHandler.ts
+++ src/http/MacaroonHttpHandler.ts
@@ -2,6 +2,10 @@ import { getLoggerFor } from '@solid/community-server';
 import type { HttpHandlerInput } from '@solid/community-server';
 import { HttpHandler } from '@solid/community-server';
 
+const MACAROON_BASE_PATH = "/.macaroon";
+const DISCHARGE_PATH = MACAROON_BASE_PATH + "/discharge";
+const FETCH_PATH = MACAROON_BASE_PATH + "/fetch";
+
 
 /**
  * HTTP handler for macaroons
@@ -13,13 +17,36 @@ export class MacaroonHttpHandler extends HttpHandler {
     super();
   }
 
+  public static isFetchURL(url:string|undefined):boolean{
+    return url!.normalize() === FETCH_PATH.normalize();
+  }
+
+  public static isDischargeURL(url: string|undefined):boolean{  
+    return url!.normalize() === DISCHARGE_PATH.normalize();
+  }
+
   public async canHandle(input: HttpHandlerInput): Promise<void> {
-      const { request } = input;
-      console.log(`Checking if request must be handled by MacaroonHttpHandler!`);
+      
+    
   }
 
 
-  public async handle({ request, response }: HttpHandlerInput): Promise<void> {
+  public async handle(input: HttpHandlerInput): Promise<void> {
     this.logger.info("Handling request for macaroons !");
+    const { request } = input;
+      const { url } = request;
+
+    // Handle fetching of macaroons
+    if(MacaroonHttpHandler.isFetchURL(url)){
+      console.log("Checking if fetching of macaroon is allowed!");
+    }
+    // Handle discharging of macaroons
+    else if(MacaroonHttpHandler.isDischargeURL(url)){
+      console.log("Checking if discharging of macaroon is allowed!");
+    }
   }
+
+
+
+
 }
